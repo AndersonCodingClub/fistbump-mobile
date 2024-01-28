1481b65e020062d7409683cb297b687db39f94aa
GregShatsman <shatsmang@gmail.com>
2024-01-28 01:08:44 -0600

    Style changes, add Multiple pages

--- App.tsx
+++ App.tsx
@@ -1,92 +1,22 @@
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
-import { useFonts } from 'expo-font';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-type BackgroundButtonProps = {
-  onPress: () => void;
-  title: string;
-  additionalBackground: string;
-};
+import LandingPage from './LandingPage';
+import SignUp from './SignUp';
 
-const BackgroundButton: React.FC<BackgroundButtonProps> = ({ onPress, title, additionalBackground }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.authButtonBase, { backgroundColor: additionalBackground }]}>
-    <Text style={styles.buttonText}>{title}</Text>
-  </TouchableOpacity>
-);
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        'Interstate': require('./assets/Interstate-Regular.ttf'),
-      });
-
     return (
-        <View style={styles.container}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.titleText}>Fistbump</Text>
-            </View>
-            <View style={styles.sloganContainer}>
-                <Text style={styles.sloganText}>Connect in the Real World</Text>
-            </View>
-            <View style={styles.buttonContainer}>
-                <BackgroundButton onPress={() => alert("Log In")} title="Log In" additionalBackground='#FEF445'></BackgroundButton>
-                <BackgroundButton onPress={() => alert("Sign Up")} title="Sign Up" additionalBackground='#F9724D'></BackgroundButton>
-            </View>
-        </View>
+        <NavigationContainer>{
+            <Stack.Navigator screenOptions={{
+                headerShown: false
+            }}>
+                <Stack.Screen name="LandingPage" component={LandingPage} />
+                <Stack.Screen name="SignUp" component={SignUp} />
+          </Stack.Navigator>
+          }</NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#6FB4EB',
-        alignItems: 'center',
-    },
-
-    titleContainer: {
-        marginTop: 135
-    },
-
-    titleText: {
-        fontSize: 75,
-        color: '#372F35',
-        fontFamily: 'Interstate',
-    },
-
-    sloganContainer: {
-        flex: 1,
-        alignItems: 'center',
-        marginTop: 125,
-     },
-
-    sloganText: {
-        fontSize: 25,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: '#372F35',
-        //fontFamily: 'Roobert',
-    },
-
-    buttonContainer: {
-        flex: 2,
-        flexDirection: 'column',
-        rowGap: 25,
-        marginTop: 40
-    },
-
-    authButtonBase: {
-        justifyContent: 'center',
-        backgroundColor: 'green',
-        alignItems: 'center',
-        paddingTop: 30,
-        paddingBottom: 30,
-        paddingLeft: 90,
-        paddingRight: 90,
-        borderRadius: 0
-    },
-
-    buttonText: {
-        fontSize: 25,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: '#372F35'
-    }
-});
---
+++ LandingPage.tsx
@@ -0,0 +1,95 @@
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { useFonts } from 'expo-font';
+import { PropsWithChildren } from 'react';
+
+type BackgroundButtonProps = {
+  onPress: () => void;
+  title: string;
+  additionalBackground: string;
+};
+
+const BackgroundButton: React.FC<BackgroundButtonProps> = ({ onPress, title, additionalBackground }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.authButtonBase, { backgroundColor: additionalBackground }]}>
+    <Text style={styles.buttonText}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const LandingPage = ({navigation}: {navigation: any}) => {
+    const [fontsLoaded] = useFonts({
+        'Roobert': require('./assets/Roobert-Regular.ttf'),
+        'Roobert-Bold': require('./assets/Roobert-Bold.otf')
+      });
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.titleText}>Fistbump</Text>
+            </View>
+            <View style={styles.sloganContainer}>
+                <Text style={styles.sloganText}>Connect in the real world.</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <BackgroundButton onPress={() => alert("login")} title="Log In" additionalBackground='#FEF445'></BackgroundButton>
+                <BackgroundButton onPress={() => navigation.navigate('SignUp')} title="Sign Up" additionalBackground='#F9724D'></BackgroundButton>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#6FB4EB',
+        alignItems: 'center',
+    },
+
+    titleContainer: {
+        marginTop: '30%'
+    },
+
+    titleText: {
+        fontSize: 75,
+        color: '#372F35',
+        fontFamily: 'Roobert-Bold',
+    },
+
+    sloganContainer: {
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 25,
+     },
+
+    sloganText: {
+        fontSize: 25,
+        textAlign: 'center',
+        color: '#372F35',
+        fontFamily: 'Roobert-Bold',
+    },
+
+    buttonContainer: {
+        flex: 2,
+        flexDirection: 'column',
+        rowGap: 25,
+        marginTop: 150,
+    },
+
+    authButtonBase: {
+        justifyContent: 'center',
+        backgroundColor: 'green',
+        alignItems: 'center',
+        paddingTop: 30,
+        paddingBottom: 30,
+        paddingLeft: 110,
+        paddingRight: 110,
+        borderRadius: 5
+    },
+
+    buttonText: {
+        fontSize: 25,
+        fontFamily: 'Roobert-Bold',
+        textAlign: 'center',
+        color: '#372F35'
+    }
+});
+
+export default LandingPage;
