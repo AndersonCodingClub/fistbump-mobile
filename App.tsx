9cc3ad3e6d1fecb45e3e94b294edc6d95bc3acf7
Zain Javaid <zainmfj@gmail.com>
2024-01-28 01:39:05 -0600

    Added seperate login/signup screens

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 type BackgroundButtonProps = {
   onPress: () => void;
@@ -13,25 +16,67 @@ const BackgroundButton: React.FC<BackgroundButtonProps> = ({ onPress, title, add
   </TouchableOpacity>
 );
 
-export default function App() {
-    const [fontsLoaded] = useFonts({
-        'Interstate': require('./assets/Interstate-Regular.ttf'),
-      });
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.titleText}>Fistbump</Text>
-            </View>
-            <View style={styles.sloganContainer}>
-                <Text style={styles.sloganText}>Connect in the Real World</Text>
-            </View>
-            <View style={styles.buttonContainer}>
-                <BackgroundButton onPress={() => alert("Log In")} title="Log In" additionalBackground='#FEF445'></BackgroundButton>
-                <BackgroundButton onPress={() => alert("Sign Up")} title="Sign Up" additionalBackground='#F9724D'></BackgroundButton>
-            </View>
+function HomeScreen({ navigation }: {navigation: any}) {
+  return (
+    <View style={styles.container}>
+        <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>Fistbump</Text>
         </View>
-    );
+        <View style={styles.sloganContainer}>
+            <Text style={styles.sloganText}>Connect in the Real World</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+            <BackgroundButton onPress={() => navigation.navigate('Log In')} title="Log In" additionalBackground='#FEF445'></BackgroundButton>
+            <BackgroundButton onPress={() => navigation.navigate('Sign Up')} title="Sign Up" additionalBackground='#F9724D'></BackgroundButton>
+        </View>
+    </View>
+  );
+}
+
+function LogInScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Log In</Text>
+    </View>
+  );
+}
+
+function SignUpScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Sign Up</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Interstate': require('./assets/Interstate-Regular.ttf'),
+  });
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="Log In"
+          component={LogInScreen} 
+          options={{ headerShown: true, headerTintColor: '#372F35', headerTransparent: true, headerTitle: "", headerBackTitle: "Back" }}
+        />
+        <Stack.Screen 
+          name="Sign Up" 
+          component={SignUpScreen} 
+          options={{ headerShown: true,  headerTintColor: '#372F35', headerTransparent: true, headerTitle: "", headerBackTitle: "Back" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
