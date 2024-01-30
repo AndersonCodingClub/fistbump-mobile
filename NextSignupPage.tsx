e7018b437d395aae04279fc17b79399439d362de
Zain Javaid <zainmfj@gmail.com>
2024-01-29 18:11:50 -0600

    Minor changes
    Co-authored-by: AKAvery <AKAvery@users.noreply.github.com>

--- NextSignupPage.tsx
+++ NextSignupPage.tsx
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
 import { useFonts } from 'expo-font';
-import React from 'react';
+import React, { useState } from 'react';
+import RNPickerSelect from 'react-native-picker-select';
+
+
 
 type BackgroundButtonProps = {
     onPress: () => void;
@@ -14,13 +17,50 @@ const BackgroundButton: React.FC<BackgroundButtonProps> = ({ onPress, title, isE
     </TouchableOpacity>
 );
 
-function NextSignupScreen() {
+const Dropdown = () => {
+    const [selectedValue, setSelectedValue] = useState(null);
+  
+    const placeholder = {
+      label: 'Select a major...',
+      value: null,
+    };
+  
+    const options = [
+      { label: 'Computer Science', value: 'Computer Science' },
+      { label: 'Business', value: 'Business' },
+      { label: 'Economics', value: 'Economics' },
+      { label: 'Biology', value: 'Biology'}
+    ];
+  
     return (
-        <Text style= {{marginLeft: 50, marginTop: 100}}>Hi besty!</Text>
+      <View>
+        <RNPickerSelect
+          placeholder={placeholder}
+          items={options}
+          onValueChange={(value) => setSelectedValue(value)}
+          value={selectedValue}
+        />
+      </View>
     );
-}
+  };
 
-const styles = StyleSheet.create({
+  const NextSignupScreen = () => {
+    return (
+        <View style={styles.container}>
+            <View style={{marginLeft: 50, marginTop: 100}}>
+                <Text style={styles.inputLabel}> Major </Text>
+            </View>
+            <View style={styles.input}>
+                <Dropdown />
+            </View>
+       </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
     authButtonBase: {
         justifyContent: 'center',
         alignItems: 'center',
@@ -32,12 +72,36 @@ const styles = StyleSheet.create({
         borderRadius: 5
     },
 
+    dropDownButtonBase: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 100,
+    },
+
     buttonText: {
         fontSize: 25,
         fontFamily: 'Roobert-Bold',
         textAlign: 'center',
         color: '#372F35'
+    },
+    input: {
+        width: 300,
+        height: 60,
+        backgroundColor: '#E6E6E6',
+        paddingLeft: 20,
+        paddingRight: 20,
+        color: '#372F35',
+        fontSize: 16,
+        marginLeft: 50
+    },
+
+    inputLabel: {
+        fontSize: 22.5,
+        color: '#372F35',
+        marginBottom: 5,
+        fontFamily: 'Roobert-Bold',
     }
+
 });
 
 export default NextSignupScreen;
